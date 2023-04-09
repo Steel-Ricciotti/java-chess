0835a042859470450e03d12021b3ee90bdd9aebe
Steel <steel.ricciotti@gmail.com>
2023-04-08 20:12:40 -0400

    Added ability to move pieces up once square. Still bugs. Can only move each piece one time.

---
+++ src/TestPiece.java
@@ -0,0 +1,37 @@
+//import javax.swing.*;
+//
+//public class TestPiece {
+//
+//    private String image;
+//    private int location;
+//    private char team;
+//
+//    public Piece( String image, int location, char team) {
+//
+//        this.image = image;
+//        this.location = location;
+//        this.team = team;
+//    }
+//
+//
+//
+//    public String getImage() {
+//        return image;
+//    }
+//
+//    public int getLocation() {
+//        return location;
+//    }
+//
+//    public void setLocation(int location) {
+//        this.location = location;
+//    }
+//
+//    public void setTeam(char team){
+//        this.team = team;
+//    }
+//
+//    public char getTeam(){
+//        return this.team;
+//    }
+//}
